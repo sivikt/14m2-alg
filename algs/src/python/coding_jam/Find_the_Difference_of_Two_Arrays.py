88e1da84ec4b87628a1d46d584400ed434bef53c
Siarhei Sintsou <Siarhei.Sintsou@burberry.com>
2024-01-03 17:06:38 +0200

    solve +1 task

--- algs/src/python/coding_jam/Find_the_Difference_of_Two_Arrays.py
+++ algs/src/python/coding_jam/Find_the_Difference_of_Two_Arrays.py
@@ -51,3 +51,40 @@ class Solution:
                 r[1].append(k)
 
         return r
+
+   def findDifference2(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        r = [[],[]]
+
+        nums1.sort()
+        nums2.sort()
+
+        i, j = 0, 0
+
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] < nums2[j]:
+                if not r[0] or r[0][-1] != nums1[i]:
+                    r[0].append(nums1[i])
+                i += 1
+            elif nums2[j] < nums1[i]:
+                if not r[1] or r[1][-1] != nums2[j]:
+                    r[1].append(nums2[j])
+                j += 1
+            else:
+                i += 1
+                j += 1
+                while i < len(nums1) and nums1[i] == nums1[i-1]:
+                    i += 1
+                while j < len(nums2) and nums2[j] == nums2[j-1]:
+                    j += 1
+
+        while i < len(nums1):
+            if nums2[-1] != nums1[i] and (not r[0] or r[0][-1] != nums1[i]):
+                r[0].append(nums1[i])
+            i += 1
+
+        while j < len(nums2):
+            if nums1[-1] != nums2[j] and (not r[1] or r[1][-1] != nums2[j]):
+                r[1].append(nums2[j])
+            j += 1
+
+        return r
