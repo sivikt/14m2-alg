86d9126114b7accfef51c080702e1dc956c9488f
Siarhei Sintsou <Siarhei.Sintsou@burberry.com>
2024-01-09 18:50:12 +0200

    solve +1 task

--- algs/src/python/coding_jam/Number_of_Recent_Calls.py
+++ algs/src/python/coding_jam/Number_of_Recent_Calls.py
@@ -30,6 +30,9 @@ Constraints:
     Each test case will call ping with strictly increasing values of t.
     At most 10^4 calls will be made to ping.
 """
+from collections import deque
+
+
 class RecentCounter:
 
     def __init__(self):
